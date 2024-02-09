57652e4f35c68beabb0da9c2521891fa9dc30488
robertomalheiros <robertomalheirosb@gmail.com>
2024-02-09 15:21:11 -0300

    Gerando qrcode

--- qrcodeteams/src/app/page.js
+++ qrcodeteams/src/app/page.js
@@ -18,16 +18,17 @@ export default function Home() {
   console.log(link)
   return (
     <main className="flex min-h-screen flex-col items-center bg-gray-100 justify-between p-24">
+            <div>
+					<h1 className="text-2xl font-semibold">GERADOR QR CODE 2ª VARA SSJ VCA</h1>
+				</div>
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+
 	<div className="relative py-3 sm:max-w-xl sm:mx-auto">
 		<div
 			className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
 		</div>
 		<div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
 			<div className="max-w-md mx-auto">
-				<div>
-					<h1 className="text-2xl font-semibold">GERADOR QR CODE 2ª VARA SSJ VCA</h1>
-				</div>
         <div>
 					<h2 className="text-2xl font-semibold">Crie seu QR Code</h2>
 				</div>
